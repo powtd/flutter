32d6eb58b57a9032691b9bcc5d8f11c9acefcfaf
ppcyel <42583572+ppcyel@users.noreply.github.com>
2021-01-09 01:34:35 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,21 +1,3 @@
-let price=20
-let fruit="mango"
-
-
-
-////////////////////////////////////
-// Values and Variables
-console.log("Jonas");
-
-
-let firstName = "Matilda";
-
-console.log(firstName);
-console.log(firstName);
-
-
-// Variable name conventions
-let jonas_matilda = "JM";
 
 
 
